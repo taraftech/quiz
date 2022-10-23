b078549cd978b43867bad919932d7295bd5bbd03
miladfallah <59369921+miladfallah@users.noreply.github.com>
2022-10-23 11:33:58 +0330

    fix bug for deleting last option service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './quiz/quiz.module';
 import { QuestionModule } from './question/question.module';
-import { OptionModule } from './option/option.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/quiz'),
     QuizModule,
     QuestionModule,
-    OptionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
