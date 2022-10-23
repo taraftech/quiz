809dab80a8f5b0834801f28e03a17834f79234ae
miladfallah <59369921+miladfallah@users.noreply.github.com>
2022-10-23 11:32:02 +0330

    delete last option service

--- src/interface/option.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-import { Question } from 'src/schemas/question.schema';
-
-export interface IOption extends Document {
-  text: string;
-  questionId: Question;
-  isCorrect: boolean;
-}
--- src/schemas/question.schema.ts
+++ src/schemas/question.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
-import { Option } from './option.schema';
 import { Quiz } from './quiz.schema';
 
 export type QuestionDocument = Question & Document;
