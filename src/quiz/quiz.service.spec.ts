17e9f5326b4fec134ecbb8adc3a49ae2dcce543d
miladfallah <59369921+miladfallah@users.noreply.github.com>
2022-11-01 16:32:15 +0330

    quiz service test done

--- src/quiz/quiz.service.spec.ts
+++ src/quiz/quiz.service.spec.ts
@@ -1,49 +1,78 @@
-import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
-import { string } from 'joi';
-import { Question } from '../schemas/question.schema';
-import { Quiz, QuizSchema } from '../schemas/quiz.schema';
 import { QuizService } from './quiz.service';
+import { CreateQuizDto } from '../dto/quiz/create-quiz.dto';
+import { Quiz } from '../schemas/quiz.schema';
+import { UpdateQuizDto } from '../dto/quiz/update-quiz.dto';
 
+class ApiServiceMock {
+  createQuiz(dto: any) {
+    return [];
+  }
+  getAllQuizes() {
+    return [];
+  }
+  getQuiz(id: string) {
+    return null;
+  }
+  updateQuiz(id: string, dto: any) {
+    return [];
+  }
+  deleteQuiz(id: string) {
+    return null;
+  }
+}
 describe('QuizService', () => {
-  let service: QuizService;
+  let quizService: QuizService;
 
-  const mockQuizModel = {
-    create: jest.fn().mockImplementation((dto) => dto),
-    save: jest
-      .fn()
-      .mockImplementation((quiz) => Promise.resolve({ id: string, ...quiz })),
-  };
-  const mockQuestionModel = {};
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        QuizService,
-        {
-          provide: getModelToken(Quiz.name),
-          useValue: mockQuizModel,
-        },
-        {
-          provide: getModelToken(Question.name),
-          useValue: mockQuestionModel,
-        },
-      ],
-    }).compile();
+    const ApiServiceProvider = {
+      provide: QuizService,
+      useClass: ApiServiceMock,
+    };
 
-    service = module.get<QuizService>(QuizService);
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [QuizService, ApiServiceProvider],
+    }).compile();
+    quizService = module.get<QuizService>(QuizService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(QuizService).toBeDefined();
   });
 
-  // it('should create a new quiz record and return that', async () => {
-  //   expect(
-  //     await service.createQuiz({ title: 'title', description: 'description' }),
-  //   ).toEqual({
-  //     id: expect.any(Number),
-  //     title: 'title',
-  //     description: 'description',
-  //   });
-  // });
+  it('should call createQuiz method with expected params', async () => {
+    const createQuizSpy = jest.spyOn(quizService, 'createQuiz');
+    const dto = new CreateQuizDto();
+    quizService.createQuiz(dto);
+    expect(createQuizSpy).toHaveBeenCalledWith(dto);
+  });
+
+  it('should call findQuiz method', async () => {
+    const findOneQuizSpy = jest.spyOn(quizService, 'getQuiz');
+    const quizId: string = '';
+    quizService.getQuiz(quizId);
+    expect(findOneQuizSpy).toHaveBeenCalledWith(quizId);
+  });
+
+  it('should call updateQuiz method with expected params', async () => {
+    const updateQuizSpy = jest.spyOn(quizService, 'updateQuiz');
+    const quizId = 'quizId';
+    const dto = new UpdateQuizDto();
+    quizService.updateQuiz(quizId, dto);
+    expect(updateQuizSpy).toHaveBeenCalledWith(quizId, dto);
+  });
+
+  it('should call getAllQuizes method', async () => {
+    const result = Quiz['test'];
+    jest.spyOn(quizService, 'getAllQuizes').mockImplementation(() => result);
+
+    expect(await quizService.getAllQuizes()).toBe(result);
+  });
+
+  it('should delete quiz by id in param', async () => {
+    const deleteQuizSpy = jest.spyOn(quizService, 'deleteQuiz');
+    const quizId = 'quizId';
+    quizService.deleteQuiz(quizId);
+    expect(deleteQuizSpy).toHaveBeenCalledWith(quizId);
+  });
 });
